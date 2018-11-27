d6ebc69dd17f5846489872f813a41c2853357c2a
rishikeshanilmishra <43986166+rishikeshanilmishra@users.noreply.github.com>
2018-11-27 14:58:38 +0530

    Add files via upload

---
+++ marklist.py
@@ -0,0 +1,17 @@
+name1=input("enter 1st student name")  # input students name
+name2=input("enter 2st student name")
+name3=input("enter 3st student name")
+english1=input("marks of english for student1") # input marks of english for each student
+english2=input("marks of english for student2")
+english3=input("marks of english for student3")
+science1=input("marks for scince for student1")  # input marks of science for each student
+science2=input("marks for scince for student2")
+science3=input("marks for scince for student3")
+maths1=input("marks of maths for student1")  # input marks of maths for each student
+maths2=input("marks of maths for student2")
+maths3=input("marks of maths for student3")
+student1=[name1,'english',english1,'science',science1,'maths',maths1] # define list for each student's marks
+student2=[name2,'english',english2,'science',science2,'maths',maths2]
+student3=[name3,'english',english3,'science',science3,'maths',maths3]
+student=[student1,student2,student3] #define a list for a total database
+print(student)
